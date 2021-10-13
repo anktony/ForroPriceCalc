535986e9d9d8a06ea4ab8a1e9fa0d0469fa8ca23
Anktony <antoneslima@gmail.com>
2021-10-13 17:08:35 -0300

    added dictionaries taxasSumup and taxasStone

---
+++ scripts/calculadora.js
@@ -0,0 +1,19 @@
+const debitoSumup = 1.9;
+const debitoStone = 1.8;
+
+var taxasSumup = [[1, 4.6],[2, 6.1], [3, 7.6], [4, 9.1], [5, 10.6], [6, 12.1], [7,13.6], [8, 15.1],
+                 [9, 16.6], [10, 18.1], [11, 19.6], [12, 21.1]];
+
+var taxasStone = [[1, 4.94],[2, 6.35], [3, 7.46], [4, 8.58], [5, 9.69], [6, 10.81], [7, 12.33], [8, 13.44],
+                 [9, 14.55], [10, 15.66], [11, 16.77], [12, 17.88]];
+
+
+let valorDaVenda = document.getElementById('valorDaVenda').value;
+let numeroDeParcelas = document.getElementById('numeroDeParcelas').value;
+
+function calcularValoresSumup(){
+    valorDaVenda += valorDaVenda * TaxasSumup[]
+}       
+
+// alert(taxasStone[0][1])
+
